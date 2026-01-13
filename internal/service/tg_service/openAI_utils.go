bdcc2bf83784d1f9d28656c46c57c3c2fe0cb2df
Your <you@example.com>
2026-01-13 21:06:34 +0300

    fix prompt

--- internal/service/tg_service/openAI_utils.go
+++ internal/service/tg_service/openAI_utils.go
@@ -1,6 +1,7 @@
 package tg_service
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 
@@ -15,10 +16,13 @@ func (srv *TgService) CreateGptTextOpenAI(postText string) (string, error) {
 		option.WithAPIKey(srv.Cfg.OpenAiAPIToken),
 	)
 
-	inputPrompt := fmt.Sprintf(
-		"Ты ведешь телеграм-канал. Твоя задача уникализировать контент, чтобы посты не были друг на друга похожи. Я буду отправлять текст, а ты его перефразируй, дели на абзацы, добавляй смайлы, чтобы получить максимально уникальный пост.\nтекст ниже:\n\n%v",
-		postText,
-	)
+	var mess bytes.Buffer
+	mess.WriteString("Ты ведешь телеграм-канал. Твоя задача уникализировать контент, чтобы посты не были друг на друга похожи.")
+	mess.WriteString("Я буду отправлять текст, а ты его перефразируй, дели на абзацы, добавляй смайлы, чтобы получить максимально уникальный пост.\n")
+	mess.WriteString("Если встретишь слова типо @lichka не нужно их изменять вообще, оставь их как есть, и не нужно добавлять никах markdown форматирований типо звездочек и прочего.\n")
+	mess.WriteString("Нужен только итоговый текст, без предисловий и твоих предложений сделать что-либо ещё в конце.\n")
+	mess.WriteString(fmt.Sprintf("текст ниже:\n\n%v", postText))
+	inputPrompt := mess.String()
 
 	resp, err := client.Responses.New(context.Background(), responses.ResponseNewParams{
 		Model: responses.ChatModelGPT4_1Mini,
